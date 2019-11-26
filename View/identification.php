281689e5beb9b283064dbd70de38ae4ec40d4017
infocoms <kevincomyn@gmail.com>
2019-11-26 16:40:17 +0100

    basic form for tourist feeback

--- View/identification.php
+++ View/identification.php
@@ -1,1 +1,21 @@
 <?php
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>AppreciRate</title>
+</head>
+<body class="text-center">
+
+    <form class="form-signin">
+        <img class="mb-4" src="/Assets/Img/Logo.jpg" alt="" width="72" height="72">
+        <h1 class="h3 mb-3 font-weight-normal">Feedback</h1>
+        <input type="text" id="inputName" class="form-control" placeholder="Name" required="" autofocus="">
+        <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Continue</button>
+        <p class="mt-5 mb-3 text-muted">© BECODE 2019</p>
+    </form>
+</body>
+</html>
