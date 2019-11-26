134decd47d311b1d2ca103c7ece6d832ff9f80d7
Manny <manny-apsel@hotmail.com>
2019-11-26 16:31:27 +0100

    input box company

--- View/companyDepartment.php
+++ View/companyDepartment.php
@@ -1,1 +1,17 @@
-<?php
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Select Company/Department</title>
+</head>
+<body>
+<form>
+    <label>
+        Select your Company
+        <input type="text" name="Company" list="company">
+    </label>
+</form>
+</body>
+</html>
