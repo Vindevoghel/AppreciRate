9a966f84bbb8cc8e7cb381a0e9422c88bb483891
JeroenDv <JeroenDV>
2019-11-27 17:01:53 +0100

    confirm added and bugs fi

--- Controller/RatingController.php
+++ Controller/RatingController.php
@@ -3,15 +3,12 @@ declare(strict_types=1);
 
 class RatingController {
 
+    function render()
+    {
 
-$rating = "";
 
-if (isset($_POST['submit'])) {
-if (isset($_POST['image_radio'])) {
-$rating = $_POST['image_radio'];  //
-    //echo "You have selected :".$_POST['radio'];  //  Displaying Selected Value
-}
-}
+        require "../View/confirm.php";
+    }
 
 
 }
