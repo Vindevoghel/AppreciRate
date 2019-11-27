999f2dc2c2b76e2cf8294d84645d1b1ef24f07b4
rafahalani <alani.rafah@yahoo.com>
2019-11-27 15:03:41 +0100

    text box is smaller now

--- View/review.php
+++ View/review.php
@@ -1,1 +1,20 @@
-<?php
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body class="text-center">
+<?php include "View/header.php"?>
+    <h3>Observation Page!</h3>
+    <img class="mb-4" src="/Assets/Img/Logo.jpg" alt="" width="72" height="72">
+    <form class="form-signin">
+        <div class="form-group">
+            <label for="comment">text:</label>
+            <textarea class="form-control" rows="5" id="comment"></textarea>
+        </div>
+    </form>
+
+    <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Submit</button>
+    <p class="mt-5 mb-3 text-muted">© BECODE 2019</p>
+
+</body>
+</html>
