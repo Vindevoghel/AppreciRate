9dd1c06b6fd359828fef0374914c4af610a24137
infocoms <kevincomyn@gmail.com>
2019-11-27 12:09:14 +0100

    added progress bar session to the header.php and session for the tourist name in the identification.php

--- View/identification.php
+++ View/identification.php
@@ -1,5 +1,13 @@
 <?php
 
+session_start();
+
+
+
+$Input = $_POST['Name'];
+
+$_SESSION["Tourist Name"] = $Input;
+$_SESSION["Progress"] = 10;
 
 ?>
 
@@ -9,13 +17,15 @@
     <title>AppreciRate</title>
 </head>
 <body class="text-center">
+<form class="form-signin" method="post">
+<?php include "View/header.php"?>
+    <img class="m-4" src="/Assets/Img/Logo.jpg" alt="" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">Feedback</h1>
+    <input type="text" id="inputName" class="form-control" placeholder="Name" name="Name" required="" autofocus="">
+    <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Continue</button>
+    <p class="mt-5 mb-3 text-muted">© BECODE 2019</p>
 
-    <form class="form-signin">
-        <img class="mb-4" src="/Assets/Img/Logo.jpg" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">Feedback</h1>
-        <input type="text" id="inputName" class="form-control" placeholder="Name" required="" autofocus="">
-        <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Continue</button>
-        <p class="mt-5 mb-3 text-muted">© BECODE 2019</p>
-    </form>
+
+</form>
 </body>
 </html>
