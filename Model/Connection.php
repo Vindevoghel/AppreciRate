293fdb999c4eccdac53e0332fe4d62a07906a695
TristanEugene <flp.vndrstrtn@gmail.com>
2019-12-03 19:05:47 +0100

    my connection and deleted some lines in index.php

--- Model/Connection.php
+++ Model/Connection.php
@@ -8,8 +8,8 @@ class Connection
     function openConnection()
     {
         $dbhost = "localhost";
-        $dbuser = "username"; //username
-        $dbpass = "test"; //pass of username
+        $dbuser = "filip"; //username
+        $dbpass = "guest"; //pass of username
         $db = "mydb"; //schema or database name
 
         $pdo = new PDO('mysql:host=' . $dbhost . ';dbname='. $db, $dbuser, $dbpass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
