313e4c2995a4be8be4c5d9797c5b92ddc4bb4496
Manny <manny-apsel@hotmail.com>
2019-12-03 14:50:21 +0100

    add review

--- Controller/IdentificationController.php
+++ Controller/IdentificationController.php
@@ -1,9 +1,22 @@
 <?php
 declare(strict_types=1);
+
 class IdentificationController
 {
     function render()
     {
+        if (isset($_SESSION["Tourist Name"]) && isset($_SESSION["Company value"]) && isset($_SESSION["Department value"]) && isset($_SESSION["Employee Value"]) /*&& isset($_SESSION["Tourst Name"])*/) {
+            $tourist = new Tourist(1, $_SESSION["Tourist Name"]);
+            $touristLoader = new TouristLoader();
+            $touristLoader->createTourist($tourist);
+
+            $date =
+
+
+            $review = new Review(1, "test", 1, 1, 1, $_SESSION["Company value"], $_SESSION["Department value"], $_SESSION["Employee Value"]);
+            $reviewLoader = new ReviewLoader();
+            $reviewLoader->createReview($review);
+        }
         require "View/identification.php";
     }
 
