f945c32c4eb1d06e49d4dc174051176b25ae9050
JeroenDv <JeroenDV>
2019-12-03 11:49:29 +0100

    just anothe smiley / endpage

--- View/endPage.php
+++ View/endPage.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 ?>
 
 <form method="post">
+    <img src="../Assets/Img/icons/ok.png" height="80"><br>
+    <p>Thank you!</p>
     <label>
         <input type="submit" name="New Review" value="New Review">
     </label>
