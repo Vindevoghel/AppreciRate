d389d9fabe9d0cfd065f10839ad027ba3440ffa7
TristanEugene <flp.vndrstrtn@gmail.com>
2019-12-05 10:39:15 +0100

    some more styling, but needs some more padding between certain elements

--- View/endPage.php
+++ View/endPage.php
@@ -6,10 +6,12 @@ $_SESSION["Progress"] = 100;
 
 <form  class="form-signin" method="post">
     <?php include "View/header.php"?>
-    <img id="endsmiley" src="../Assets/Img/icons/ok.png" height="80"><br>
+    <img id="endsmiley" src="../Assets/Img/icons/ok.png" height="90"><br>
     <p>Thank you!</p>
-    <input type="submit" class="form-control text-center mb-3 endpagebutton" placeholder="Please enter your name"
+    <input type="submit" class="form-control text-center mb-3 endpagebutton"
            name="home" value="Home">
-    <input type="submit" class="form-control text-center endpagebutton" placeholder="Please enter your name" name="abt"
+    <input type="submit" class="form-control text-center endpagebutton" name="abt"
            value="About Us">
+
+    <p class="mt-5 mb-3 text-muted">© BeCode 2019</p>
 </form>
--- View/review.php
+++ View/review.php
@@ -10,13 +10,13 @@ $_SESSION["Progress"] = 75;
     <div class="container">
         <form class="form-signin" method="post">
             <?php include "header.php"?>
-            <h3>Feedback for this person</h3>
+            <h3>What's your observation <br/> for this person?</h3>
 
             <div class="form-group pt-3">
                 <textarea class="form-control" rows="5" id="comment" name="comment" maxlength="500"></textarea>
             </div>
 
-            <button type="submit">Submit</button>
+            <button type="submit">Continue</button>
             <p class="mt-5 mb-3 text-muted">© BeCode 2019</p>
         </form>
     </div>
